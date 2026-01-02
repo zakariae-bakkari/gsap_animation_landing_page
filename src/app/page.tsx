8b63e3b0e7e0f8cb62927a3c090e74921dd46f67
Zakariae Bakkari <zakariabakkari2006@gmail.com>
2026-01-02 18:02:07 +0100

    Add Lenis smooth scrolling integration and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
-import About from '@src/components/about';
-import { Art } from '@src/components/art';
-import Cocktails from '@src/components/cocktails';
-import { Contact } from '@src/components/contact';
-import Hero from '@src/components/hero';
-import { Menu } from '@src/components/menu';
-import { Navbar } from '@src/components/navbar';
-import gsap from 'gsap';
-import { ScrollTrigger,SplitText } from 'gsap/all';
-
-gsap.registerPlugin(ScrollTrigger,SplitText);
+"use client";
+import About from "@src/components/about";
+import { Art } from "@src/components/art";
+import Cocktails from "@src/components/cocktails";
+import { Contact } from "@src/components/contact";
+import Hero from "@src/components/hero";
+import { Menu } from "@src/components/menu";
+import { Navbar } from "@src/components/navbar";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Lenis from "lenis";
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Home() {
+  // create lenis instance
+  const lenis = new Lenis({
+    duration: 2,// smooth scroll duration
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  });
+  // on scroll event 
+  lenis.on("scroll", () => {
+    // connect lenis with scrolltrigger
+    ScrollTrigger.update();
+  });
+
+  // raf loop that drives lenis (request animation frame)
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
   return (
     <main>
       <Navbar />
